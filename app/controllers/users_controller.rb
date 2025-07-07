d620e2884fd6ad8537fe0c9c80adad4be4a19d55
Martine9670 <pinnamartine@free.fr>
2025-07-07 15:31:25 +0200

    add all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,50 @@
 class UsersController < ApplicationController
 
-  # Action pour afficher la liste de tous les utilisateurs
+  # Liste des utilisateurs
   def index
     @users = User.all
   end
 
-  # Action pour afficher un utilisateur spécifique
+  # Affiche un utilisateur
   def show
     @user = User.find(params[:id])
   end
 
+  # Formulaire d’inscription
   def new
     @user = User.new
   end
 
+  # Création utilisateur
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to @user, notice: "Utilisateur créé avec succès !"
     else
       Rails.logger.info(@user.errors.full_messages)
-      render :new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Formulaire édition utilisateur
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # Mise à jour utilisateur
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Profil mis à jour !"
+    else
+      Rails.logger.info(@user.errors.full_messages)
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :city_id)
+    params.require(:user).permit(:first_name, :last_name, :description, :age, :city_id, :email, :password, :password_confirmation)
   end
 end
